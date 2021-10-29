e03679b274040a98e3264ffe0cd9fc5d4e98c258
V-mari <violettrujillo5@gmail.com>
2021-10-29 12:49:57 -0400

    concecionaria bd

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import util.ConectorBD;
+
+import java.sql.Connection;
+
+public class Main {
+    public static  void main(String[] args){
+        Connection connection = ConectorBD.getConnection();
+
+    }
+}
